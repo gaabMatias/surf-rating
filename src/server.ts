70fa230b7f1fd3e0741a05ed29ae0d6b43f18965
Gabriel Matias Corrêa <gabrielmatias0202@gmail.com>
2021-10-13 14:53:24 -0300

    database implement & setup commit

--- src/server.ts
+++ src/server.ts
@@ -4,14 +4,16 @@ import bodyParser from 'body-parser';
 
 import { ForecastController } from './controller/forecast';
 import { Application } from 'express';
+import * as database from '@src/database'
 
 export class SetupServer extends Server {
   constructor(port = 3000) {
     super();
   }
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
+    await this.setupDatabase()
   }
 
   private setupExpress(): void {
@@ -22,6 +24,14 @@ export class SetupServer extends Server {
     const forecastController = new ForecastController();
 
     this.addControllers([forecastController]);
+  }
+
+  private async setupDatabase(): Promise<void> {
+    await database.connect()
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
   }
 
   public getApp(): Application {
